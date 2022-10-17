350fd151f1253c144e60ed73a2bca614fe44219b
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-10-17 18:06:16 +0000

    html

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@
 // This file contains the JS functions for index.html
 
 "use strict"
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function calculate () {
+  // input
+  const length = parseInt(document.getElementById('length-of-rectangle').value)
+  const width = parseInt(document.getElementById('width-of-rectangle').value)
+
+  // process
+  const area = length * width
+  const perimeter = 2 * (length + width)
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+  document.getElementById('perimeter').innerHTML = 'Perimeter is: ' + perimeter + ' cm'
+}
