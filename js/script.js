2c79ef925045272d3baa5e778860f72ed0a831a1
sophie-nguyen-0 <sophie-nguyen-0@users.noreply.github.com>
2022-10-17 18:23:47 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -6,16 +6,17 @@
 /**
  * This function calculates area and perimeter of rectangle.
  */
-function calculate () {
+function calculate() {
   // input
-  const length = parseInt(document.getElementById('length-of-rectangle').value)
-  const width = parseInt(document.getElementById('width-of-rectangle').value)
+  const length = parseInt(document.getElementById("length-of-rectangle").value)
+  const width = parseInt(document.getElementById("width-of-rectangle").value)
 
   // process
   const area = length * width
   const perimeter = 2 * (length + width)
 
   // output
-  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
-  document.getElementById('perimeter').innerHTML = 'Perimeter is: ' + perimeter + ' cm'
+  document.getElementById("area").innerHTML = "Area is: " + area + " cm²"
+  document.getElementById("perimeter").innerHTML =
+    "Perimeter is: " + perimeter + " cm"
 }
